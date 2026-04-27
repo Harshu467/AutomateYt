cfd46fcda6b1bc0e61f780ce647c568382ed1b68
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2026-04-27 22:43:30 +0530

    Add User entity

---
+++ backend/Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+namespace Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; } = "";
+    public string PasswordHash { get; set; } = "";
+    public string Plan { get; set; } = "Free";
+}
